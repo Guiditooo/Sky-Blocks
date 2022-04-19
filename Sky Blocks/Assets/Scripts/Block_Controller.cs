c34987a049d8e332ad11a319c822e289371009bb
Guido Tello <lpmaleinn@gmail.com>
2022-04-19 03:24:13 -0300

    Added BlockController script
    This may destroy the joint and make the block fall.
    (On Going) Then, freeze rotations and positions.

---
+++ Sky Blocks/Assets/Scripts/Block_Controller.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block_Controller : MonoBehaviour
+{
+
+    private HingeJoint joint;
+
+    private Rigidbody rb;
+
+    private bool justStopped;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        joint = GetComponent<HingeJoint>();
+    }
+
+    public void DropBlock()
+    {
+        Destroy(joint);
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        //StartCoroutine(MovementCheck(collision.gameObject));
+        if (!collision.gameObject.CompareTag("Limits"))
+        {
+            if (transform.eulerAngles.x % 90 >= 0 && transform.eulerAngles.x % 90 <= 1)
+            {
+                if (transform.eulerAngles.y % 90 >= 0 && transform.eulerAngles.y % 90 <= 1)
+                {
+                    if (transform.eulerAngles.z % 90 >= 0 && transform.eulerAngles.z % 90 <= 1)
+                    {
+
+                        rb.constraints = RigidbodyConstraints.FreezeAll;
+
+                    }
+                }
+            }
+
+        }
+    }
+    /*
+    IEnumerator MovementCheck(GameObject go)
+    {
+        if (!go.CompareTag("Limits"))
+        {
+            if (transform.eulerAngles.x % 90 >= 0 && transform.eulerAngles.x % 90 <= 1)
+            {
+                if (transform.eulerAngles.y % 90 >= 0 && transform.eulerAngles.y % 90 <= 1)
+                {
+                    if (transform.eulerAngles.z % 90 >= 0 && transform.eulerAngles.z % 90 <= 1)
+                    {
+
+                        justStopped = true;
+
+                        yield return justStopped;
+
+                    }
+                }
+            }
+
+        }
+
+    }
+    */
+
+}
